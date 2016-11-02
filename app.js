2ab3f7b6b6dc5545715a6a28fdbfa5f92e99a633
h0ly byte <admin@kbve.com>
2016-11-02 04:27:48 +0000

    Online

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+console.log("Starting");
 var express = require('express');
 var app = express();
 var path = require('path');
@@ -5,3 +6,4 @@ app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
 app.listen(1337);
+console.log("Online");
